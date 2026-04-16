5312ecee098304f76e9d167dd44591c041b7a02a
apvincent-jpg <apvincent@ucdavis.edu>
2026-04-16 10:34:33 -0700

    blockbuster

--- studio1/script.js
+++ studio1/script.js
@@ -1,10 +1,29 @@
 (function () {
 
-    const fs = document.querySelector('.fa-expand-alt');
-
     console.log('reading js');
+    "use strict";
 
-    fs.addEventListener('click', function() {
+    const fs = document.querySelector('.fa-expand-alt');
+    const video1 = document.querySelector('#video1');
+    const section = document.querySelector("section");
+    // const type = document.querySelector("p"); // not needed
+    const intervalID = setInterval(checkTime, 10000);
+
+    const line1 = document.querySelector('.line1');
+    const line2 = document.querySelector('.line2');
+    const line3 = document.querySelector('.line3');
+    const line4 = document.querySelector('.line4');
+
+    const lines = document.querySelectorAll("section div");
+    const img = document.querySelector('img');
+    let canChange = true;
+
+    // hide all lines at start
+    lines.forEach(function (line) {
+        line.className = "hidden";
+    });
+
+    fs.addEventListener('click', function () {
         if (!document.fullscreenElement) {
             document.documentElement.requestFullscreen();
         } else {
@@ -12,27 +31,70 @@
         }
     });
 
-const video1 = document.querySelector('#video1');
-const section = document.querySelector("section");
-const type = document.querySelector("p");
-const img1 = document.querySelector("img");
+    //random type function
+    function randomType(container) {
 
-const intervalID = setInterval(checkTime, 1000);
+        if (!canChange) return;
+        const lines = container.querySelectorAll("p");
 
-type.className = "hidden";
-img1.className = "hidden"; 
-
-function checkTime() {
-    if (1 < video1.currentTime && video1.currentTime < 3) {
-        type.className = "showing";
-    } else {
-        type.className = "hidden";
+        lines.forEach(function (line) {
+            line.style.position = "absolute";
+            line.style.top = Math.random() * 80 + "vh";
+            line.style.left = Math.random() * 80 + "vw";
+            // line.style.color = "white";
+            line.style.zIndex = "10";
+        });
     }
-    if (5 < video1.currentTime && video1.currentTime < 7) {
-        img1.className = "showing";
-    } else {
-        img1.className = "hidden";
+    //check time
+    function checkTime() {
+
+        lines.forEach(function (line) {
+            line.classList.remove("showing");
+            line.classList.add("hidden");
+        });
+
+        img.classList.remove("showing");
+        img.classList.add("hidden");
+
+        const t = video1.currentTime;
+
+        if (t > 2.5 && t < 7) {
+            img.classList.remove("hidden");
+            img.classList.add("showing");
+        }
+
+        else if (t >= 8 && t < 12) {
+            line1.classList.remove("hidden");
+            line1.classList.add("showing");
+            randomType(line1);
+        }
+
+        else if (t >= 12 && t < 18) {
+            line2.classList.remove("hidden");
+            line2.classList.add("showing");
+            randomType(line2);
+        }
+
+        else if (t >= 18 && t < 22) {
+            line3.classList.remove("hidden");
+            line3.classList.add("showing");
+            randomType(line3);
+        }
+
+        else if (t >= 22 && t < 28) {
+            line4.classList.remove("hidden");
+            line4.classList.add("showing");
+            randomType(line4);
+        }
     }
-}
+
+    video1.addEventListener("timeupdate", checkTime);
+
+
+    //grayscale switch 
+    document.addEventListener("click", function () {
+    video1.classList.toggle("grayscaleSwitch");
+});
+    
+
 })();
-

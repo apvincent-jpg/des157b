b41b2902bfe065f88ddeaa4b0d195f2834de039e
apvincent-jpg <apvincent@ucdavis.edu>
2026-05-05 20:53:47 -0700

    liberty library

---
+++ studio3/script.js
@@ -0,0 +1,47 @@
+//library 
+
+let imgs = [];
+
+function preload() {
+  // Load the image files.
+  imgs[0] = loadImage('/studio3/images/lib_beetle.png');
+  imgs[1] = loadImage('/studio3/images/lib_person.png');
+  imgs[2] = loadImage('/studio3/images/lib_star.png');
+  imgs[3] = loadImage('/studio3/images/lib_mountain.png');
+
+}
+
+function setup() {
+  createCanvas(1200, 750, WEBGL);
+  angleMode(DEGREES);
+  imageMode(CENTER);
+}
+
+function draw() {
+  background(0, 93, 152);
+  orbitControl();
+
+  for (let zAngle = 0; zAngle < 180; zAngle += 30) {
+
+    // one image per FULL ring
+    let layerIndex = zAngle / 30;
+    let currentImg = imgs[layerIndex % imgs.length];
+
+    for (let xAngle = 0; xAngle < 360; xAngle += 10) {
+      push();
+
+      rotateZ(zAngle);
+      rotateX(xAngle);
+
+      translate(0, 400, 0);
+
+      // keep images flat
+      rotateX(-xAngle);
+      rotateZ(-zAngle);
+
+      image(currentImg, 0, 0, 50, 50);
+
+      pop();
+    }
+  }
+}
